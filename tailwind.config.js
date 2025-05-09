a022a34e85da0a1bf351c845c24441b792ccf7ee
jiin-seok-intellifam <206485553+jiin-seok-intellifam@users.noreply.github.com>
2025-05-09 14:51:07 +0900

    feat: 반응형 화면 나누기 포인트 지정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 const config = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
+    screens: {
+      'mobile': '375px',
+      'tablet': '768px',
+      'desktop': '1440px',
+    },
     extend: {
       colors: {
         primary: '#1e40af',
