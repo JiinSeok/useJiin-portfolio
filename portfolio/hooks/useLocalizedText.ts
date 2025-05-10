d968de520a5e301310704595b49d1a8152e1a22c
JiinSeok <126224323+JiinSeok@users.noreply.github.com>
2025-05-10 20:32:19 +0900

    chore: 미사용 파일 삭제

--- portfolio/hooks/useLocalizedText.ts
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import { useIntl } from "react-intl";
-
-export const useLocalizedText = () => {
-  const { formatMessage } = useIntl();
-  return (id: string, values?: Record<string, string | number>) =>
-    formatMessage({ id }, values);
-};
