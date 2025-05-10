4f7182696a5bd5213f9d6802a01087b387ecae82
JiinSeok <126224323+JiinSeok@users.noreply.github.com>
2025-05-10 20:59:27 +0900

    feat: metadata 분리

---
+++ src/app/config.ts
@@ -0,0 +1,15 @@
+export const metaData = {
+  baseUrl: 'https://jiin-seok.net/',
+  title: '석지인 Jiin Seok',
+  name: '석지인',
+  ogImage: '/profile.jpg',
+  description: '프론트엔드 개발자 석지인의 사이트입니다.',
+}
+
+export const socialLinks = {
+  // twitter: "https://x.com/1tssirius",
+  github: 'https://github.com/JiinSeok',
+  instagram: 'https://www.instagram.com/browhnair',
+  linkedin: 'https://www.linkedin.com/in/jiin-seok',
+  email: 'mailto:seokjiin1073@gmail.com',
+}
